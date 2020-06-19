b0798d1ee9fc2fe2fb91549793c6ae8fb6f4ca42
fanqieVip <42194904+fanqieVip@users.noreply.github.com>
2020-06-19 09:29:02 +0800

    回滚到1.1.20版本
    21版本出现了更多不可预知的问题

--- src/main/java/com/fanjun/keeplive/service/JobHandlerService.java
+++ src/main/java/com/fanjun/keeplive/service/JobHandlerService.java
@@ -1,96 +1,87 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by Fernflower decompiler)
+//
+
 package com.fanjun.keeplive.service;
 
 import android.app.Notification;
-import android.app.job.JobInfo;
 import android.app.job.JobParameters;
 import android.app.job.JobScheduler;
 import android.app.job.JobService;
+import android.app.job.JobInfo.Builder;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
-import android.os.Build;
+import android.os.Build.VERSION;
 import android.support.annotation.RequiresApi;
-
 
 import com.fanjun.keeplive.KeepLive;
 import com.fanjun.keeplive.config.NotificationUtils;
 import com.fanjun.keeplive.receiver.NotificationClickReceiver;
 import com.fanjun.keeplive.utils.ServiceUtils;
 
-/**
- * 定时器
- * 安卓5.0及以上
- */
-@RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
-@SuppressWarnings(value = {"unchecked", "deprecation"})
+@RequiresApi(
+        api = 21
+)
 public final class JobHandlerService extends JobService {
     private JobScheduler mJobScheduler;
     private int jobId = 100;
 
-    @Override
+    public JobHandlerService() {
+    }
+
     public int onStartCommand(Intent intent, int flags, int startId) {
-        startService(this);
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            mJobScheduler = (JobScheduler) getSystemService(Context.JOB_SCHEDULER_SERVICE);
-            mJobScheduler.cancel(jobId);
-            JobInfo.Builder builder = new JobInfo.Builder(jobId,
-                    new ComponentName(getPackageName(), JobHandlerService.class.getName()));
-            if (Build.VERSION.SDK_INT >= 24) {
-                builder.setMinimumLatency(JobInfo.DEFAULT_INITIAL_BACKOFF_MILLIS); //执行的最小延迟时间
-                builder.setOverrideDeadline(JobInfo.DEFAULT_INITIAL_BACKOFF_MILLIS);  //执行的最长延时时间
-                builder.setMinimumLatency(JobInfo.DEFAULT_INITIAL_BACKOFF_MILLIS);
-                builder.setBackoffCriteria(JobInfo.DEFAULT_INITIAL_BACKOFF_MILLIS, JobInfo.BACKOFF_POLICY_LINEAR);//线性重试方案
+        this.startService(this);
+        if (VERSION.SDK_INT >= 21) {
+            this.mJobScheduler = (JobScheduler) this.getSystemService("jobscheduler");
+            this.mJobScheduler.cancel(this.jobId);
+            Builder builder = new Builder(this.jobId, new ComponentName(this.getPackageName(), JobHandlerService.class.getName()));
+            if (VERSION.SDK_INT >= 24) {
+                builder.setMinimumLatency(30000L);
+                builder.setOverrideDeadline(30000L);
+                builder.setMinimumLatency(30000L);
+                builder.setBackoffCriteria(30000L, 0);
             } else {
-                builder.setPeriodic(JobInfo.DEFAULT_INITIAL_BACKOFF_MILLIS);
-                builder.setRequiresDeviceIdle(true);
+                builder.setPeriodic(30000L);
             }
-            builder.setRequiredNetworkType(JobInfo.NETWORK_TYPE_ANY);
-            builder.setRequiresCharging(true); // 当插入充电器，执行该任务
+
+            builder.setRequiredNetworkType(1);
             builder.setPersisted(true);
-            mJobScheduler.schedule(builder.build());
+            this.mJobScheduler.schedule(builder.build());
         }
-        return START_STICKY;
+
+        return 1;
     }
 
     private void startService(Context context) {
-        //启动本地服务
-        Intent localIntent = new Intent(context, LocalService.class);
-        //启动守护进程
-        Intent guardIntent = new Intent(context, RemoteService.class);
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            context.startForegroundService(localIntent);
-            context.startForegroundService(guardIntent);
-        } else {
-            context.startService(localIntent);
-            context.startService(guardIntent);
+        Intent localIntent;
+        if (VERSION.SDK_INT >= 26 && KeepLive.foregroundNotification != null) {
+            localIntent = new Intent(this.getApplicationContext(), NotificationClickReceiver.class);
+            localIntent.setAction("CLICK_NOTIFICATION");
+            Notification notification = NotificationUtils.createNotification(this, KeepLive.foregroundNotification.getTitle(), KeepLive.foregroundNotification.getDescription(), KeepLive.foregroundNotification.getIconRes(), localIntent);
+            this.startForeground(13691, notification);
         }
 
-        //使用 startForegroundService 启动service 后需要在5秒内 调用startoreground 否则会抛出ANR 异常
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            if (KeepLive.foregroundNotification != null) {
-                Intent intent2 = new Intent(getApplicationContext(), NotificationClickReceiver.class);
-                intent2.setAction(NotificationClickReceiver.CLICK_NOTIFICATION);
-                Notification notification = NotificationUtils.createNotification(this, KeepLive.foregroundNotification.getTitle(), KeepLive.foregroundNotification.getDescription(), KeepLive.foregroundNotification.getIconRes(), intent2);
-                startForeground(13691, notification);
-            }else {
-                startForeground(1, new Notification());
-            }
-        }
+        localIntent = new Intent(context, LocalService.class);
+        Intent guardIntent = new Intent(context, RemoteService.class);
+        this.startService(localIntent);
+        this.startService(guardIntent);
     }
 
-    @Override
     public boolean onStartJob(JobParameters jobParameters) {
-        if (!ServiceUtils.isServiceRunning(getApplicationContext(), "com.fanjun.keeplive.service.LocalService") || !ServiceUtils.isRunningTaskExist(getApplicationContext(), getPackageName() + ":remote")) {
-            startService(this);
+        if (!ServiceUtils.isServiceRunning(this.getApplicationContext(), "com.fanjun.keeplive.service.LocalService") || !ServiceUtils.isRunningTaskExist(this.getApplicationContext(), this.getPackageName() + ":remote")) {
+            this.startService(this);
         }
+
         return false;
     }
 
-    @Override
     public boolean onStopJob(JobParameters jobParameters) {
-        if (!ServiceUtils.isServiceRunning(getApplicationContext(), "com.fanjun.keeplive.service.LocalService") || !ServiceUtils.isRunningTaskExist(getApplicationContext(), getPackageName() + ":remote")) {
-            startService(this);
+        if (!ServiceUtils.isServiceRunning(this.getApplicationContext(), "com.fanjun.keeplive.service.LocalService") || !ServiceUtils.isRunningTaskExist(this.getApplicationContext(), this.getPackageName() + ":remote")) {
+            this.startService(this);
         }
+
         return false;
     }
 }
